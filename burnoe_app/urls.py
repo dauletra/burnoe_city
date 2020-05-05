96cc3c825363681520dd2f09697b919495f3a738
Daulet Rakhmankul <daulet.rakhmankul@gmail.com>
2020-05-05 20:23:53 +0600

    advert_detail paraqwasy zhasaldy

--- burnoe_app/urls.py
+++ burnoe_app/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('market', views.ProductList.as_view(), name='products'),
-    path('services', views.ServiceList.as_view(), name='services')
+    path('market', views.ProductList.as_view(), name='product_list'),
+    path('market/<int:pk>', views.ProductDetail.as_view(), name='product_detail'),
+    path('services', views.ServiceList.as_view(), name='service_list'),
+    path('services/<int:pk>', views.ServiceDetail.as_view(), name='service_detail')
 ]
--- burnoe_app/views.py
+++ burnoe_app/views.py
@@ -1,7 +1,7 @@
 from django.utils.timezone import now
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Contact, Product, ProductCategory, Service, ServiceCategory
 
@@ -37,7 +37,7 @@ def home(request):
 class ProductList(ListView):
     context_object_name = 'products'
     queryset = Product.objects.filter(last_date__gt=now())
-    template_name = 'market.html'
+    template_name = 'market_list.html'
     paginate_by = 6
 
     def get_context_data(self, *, object_list=None, **kwargs):
@@ -47,14 +47,42 @@ class ProductList(ListView):
         return res
 
 
+class ProductDetail(DetailView):
+    template_name = 'advert_detail.html'
+    queryset = Product.objects.filter(last_date__gt=now())
+    context_object_name = 'advert'
+
+    def get_context_data(self, **kwargs):
+        res = super().get_context_data(**kwargs)
+        obj = Product.objects.get(pk=self.kwargs.get('pk'))
+        res['photos'] = obj.productphoto_set.all()
+        res['other_adverts'] = Product.objects.filter(last_date__gt=now(), category=obj.category.id).exclude(id=obj.id)[:4]
+        res['name'] = 'product'
+        return res
+
+
 class ServiceList(ListView):
     context_object_name = 'services'
     queryset = Service.objects.filter(last_date__gt=now())
-    template_name = 'services.html'
+    template_name = 'service_list.html'
     paginate_by = 6
 
     def get_context_data(self, *, object_list=None, **kwargs):
         res = super().get_context_data(**kwargs)
         res['service_categories'] = ServiceCategory.objects.all()
         res['service_count'] = Service.objects.filter(last_date__gt=now()).count()
+        return res
+
+
+class ServiceDetail(DetailView):
+    template_name = 'advert_detail.html'
+    queryset = Service.objects.filter(last_date__gt=now())
+    context_object_name = 'advert'
+
+    def get_context_data(self, **kwargs):
+        res = super().get_context_data(**kwargs)
+        obj = Service.objects.get(pk=self.kwargs.get('pk'))
+        res['photos'] = obj.servicephoto_set.all()
+        res['other_adverts'] = Service.objects.filter(last_date__gt=now(), category=obj.category.id).exclude(id=obj.id)[:4]
+        res['name'] = 'service'
         return res

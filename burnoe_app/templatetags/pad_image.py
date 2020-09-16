a9984403c09199435a0d8f9335c648b20f730a9d
Daulet Rakhmankul <daulet.rakhmankul@gmail.com>
2020-09-16 17:16:39 +0600

    modal-carousel height improved

--- burnoe_app/templatetags/pad_image.py
+++ burnoe_app/templatetags/pad_image.py
@@ -10,4 +10,4 @@ def pad_image(photo, args):
     width, height = [int(arg) for arg in args.split(',')]
     if photo is None:
         return f'https://via.placeholder.com/{width}x{height}?text=Без фото'
-    return photo.image.build_url(width=width, height=height, crop='pad', background="auto:predominant_gradient:2")
+    return photo.image.build_url(width=width, height=height, crop='fit')
